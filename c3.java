ea5e6f1556800ea0ab61189c36539d988145999e
charliep19 <charliepenny33@outlook.com>
2018-03-21 15:29:41 -0400

    first

---
+++ c3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class c3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan= new Scanner(System.in);
+
+
+	int[] numlist=new int[3];
+	
+
+
+	for( int i=0; i<numlist.length;i++) {
+	System.out.println("Enter a number");
+	numlist[i]=scan.nextInt();
+	}
+
+}
+}
---
+++ c5.java
@@ -0,0 +1,16 @@
+
+public class c5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String[] names= {"John","Alex","Hector"};
+
+for(int i=0; i< names.length; i++) {
+	
+	if(names[i].charAt(0)=='J') {
+System.out.println(names[i]);
+}
+	}
+
+}
+}
